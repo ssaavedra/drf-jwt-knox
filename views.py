835ba6166ad81a14dd9f59bceac4584e436b1e82
Santiago Saavedra <ssaavedra@gpul.org>
2016-08-09 20:14:19 +0200

    Add documentation to views

--- views.py
+++ views.py
@@ -14,10 +14,22 @@ from jwt_knox.utils import create_auth_token
 response_payload_handler = api_settings.JWT_RESPONSE_PAYLOAD_HANDLER
 
 class JWTKnoxAPIView(APIView):
+    """
+    Base class for JWT-Knox tokens. Redefines authentication and
+    permission classes so that these views can only be authenticated
+    with a JWT Knox token.
+    """
     authentication_classes = (JSONWebTokenKnoxAuthentication,)
     permission_classes = (IsAuthenticated,)
 
 class LoginView(APIView):
+    """
+    This view authenticates a user via the
+    JWT_LOGIN_AUTHENTICATION_CLASSES (which, in turn, defaults to
+    rest_framework's DEFAULT_AUTHENTICATION_CLASSES) to get a view
+    token.
+
+    """
     authentication_classes = api_settings.JWT_LOGIN_AUTHENTICATION_CLASSES
     permission_classes = (IsAuthenticated,)
 
@@ -39,18 +51,32 @@ class VerifyView(JWTKnoxAPIView):
         return self.get(request)
 
 class DebugVerifyTokenView(JWTKnoxAPIView):
+    """
+    This view returns internal data on the token, the user and the current
+    request.
+    NOT TO BE USED IN PROUDCTION.
+    """
     def get(self, request):
         token = request.auth[0]
         return Response(response_payload_handler(token, request.user, request),
                         status=status.HTTP_200_OK)
 
 class LogoutView(JWTKnoxAPIView):
+    """
+    Invalidates the current token, so that it cannot be used anymore
+    for authentication.
+    """
     def post(self, request):
         request.auth[1].delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
 
 
 class LogoutOtherView(JWTKnoxAPIView):
+    """
+    Invalidates all the tokens except the current one, so that all other
+    remaining open sessions get closed and only the current one is still
+    open.
+    """
     def post(self, request):
         tokens_to_delete = request.user.auth_token_set.exclude(pk=request.auth[1].pk)
         num = tokens_to_delete.delete()
@@ -58,6 +84,11 @@ class LogoutOtherView(JWTKnoxAPIView):
 
 
 class LogoutAllView(JWTKnoxAPIView):
+    """
+    Invalidates all currently valid tokens for the user, including the
+    current session. This endpoint invalidates the current token, and you
+    will need to authenticate again.
+    """
     def post(self, request):
         request.user.auth_token_set.all().delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
