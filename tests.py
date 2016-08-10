fe68a644ff3d1a6f7ac34bead4519cc475a4c2ed
Santiago Saavedra <ssaavedra@gpul.org>
2016-08-10 18:50:04 +0200

    xea-core: JWT-Knox: Refactor tests
    * Refactor URL location
    
      This way we cache the reversed URL for every test (which is nice in
      case a route changes its name at some point).
    
    * Create helper functions for client API
    
      Improves readability of the tests by having a couple of verb functions
      for each API verb, so that rest_framework's APIClient calls do not
      diminish the test readability.
    
    * Create test user before each test
    
      As we usually need an user to test the tokens, we create it as a
      fixture before each test.

--- tests.py
+++ tests.py
@@ -13,18 +13,57 @@ class APIAuthTest(APITestCase):
     password = 'usuario123'
     test_logout_all_client_number = 3
 
-    def create_test_user(self):
+    # Used URLs
+    login_url = reverse('jwt_knox:get_new_token')
+    verify_url = reverse('jwt_knox:verify_token')
+    logout_current_url = reverse('jwt_knox:logout_current_token')
+    logout_other_url = reverse('jwt_knox:logout_other_tokens')
+    logout_all_url = reverse('jwt_knox:logout_all_user_tokens')
+
+    def setUp(self):
+        """
+        Before each test, run the create_test_user fixture
+        """
+        self.fixture_create_test_user()
+
+    def fixture_create_test_user(self):
         """
         Adds the default user to the database
         :return:
         """
         User.objects.create_user(username=self.username, email=self.email, password=self.password)
 
+    def with_token(self, token):
+        if not token:
+            self.client.credentials(HTTP_AUTHORIZATION=None)
+            return self
+
+        auth_header = "{prefix} {token}".format(
+            prefix=api_settings.JWT_AUTH_HEADER_PREFIX,
+            token=token
+        )
+
+        self.client.credentials(HTTP_AUTHORIZATION=auth_header)
+
+        return self
+
+    def verify_token(self, token):
+        return self.with_token(token).client.post(self.verify_url)
+
+    def logout_current(self, token=None):
+        if token:
+            self.with_token(token)
+        return self.client.post(self.logout_current_url)
+
+    def logout_other(self):
+        return self.client.post(self.logout_other_url)
+
+    def logout_all(self):
+        return self.client.post(self.logout_all_url)
+
     def get_token(self):
-        self.create_test_user()
-        myurl = reverse('jwt_knox:get_new_token')
         self.client.login(username=self.username, password=self.password)
-        response = self.client.post(myurl)
+        response = self.client.post(self.login_url)
         return response
 
     def get_n_tokens(self,n):
@@ -33,12 +72,10 @@ class APIAuthTest(APITestCase):
         :param n: Number of tokens we want to get
         :return:
         """
-        self.create_test_user()
         response_list = []
-        myurl = reverse('jwt_knox:get_new_token')
         self.client.login(username=self.username, password=self.password)
         for i in range (0,n):
-            response = self.client.post(myurl)
+            response = self.client.post(self.login_url)
             self.assertEqual(response.status_code, status.HTTP_200_OK)
             response_list.append(response)
         return response_list
@@ -67,11 +104,9 @@ class APIAuthTest(APITestCase):
         Verify that our 'logout' endpoint is logging out properly
         :return:
         """
-        url = reverse('jwt_knox:logout_current_token')
         login_response = self.get_token()
         token = login_response.data['token']
-        self.client.credentials(HTTP_AUTHORIZATION=self.build_auth_header(token))
-        logout_response = self.client.post(url)
+        logout_response = self.with_token(token).logout_current()
         self.assertEqual(logout_response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_double_login(self):
@@ -92,13 +127,11 @@ class APIAuthTest(APITestCase):
         Here we prove that after logging out once, the endpoint will reject a second logout request
         :return:
         """
-        url = reverse('jwt_knox:logout_current_token')
         login_response = self.get_token()
         self.assertEqual(login_response.status_code, status.HTTP_200_OK)
         token = login_response.data['token']
-        self.client.credentials(HTTP_AUTHORIZATION=self.build_auth_header(token))
-        logout_response1 = self.client.post(url, {}, format='json')
-        logout_response2 = self.client.post(url, {}, format='json')
+        logout_response1 = self.with_token(token).logout_current()
+        logout_response2 = self.with_token(token).logout_current()
         self.assertEqual(logout_response1.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(logout_response2.status_code, status.HTTP_401_UNAUTHORIZED)
 
@@ -107,13 +140,10 @@ class APIAuthTest(APITestCase):
         This test verifies that after logging out, the token we used becomes invalid
         :return:
         """
-        logout_url = reverse ('jwt_knox:logout_current_token')
-        verify_url = reverse ('jwt_knox:verify_token')
         login_response = self.get_token()
         token = login_response.data['token']
-        self.client.credentials(HTTP_AUTHORIZATION=self.build_auth_header(token))
-        logout_response = self.client.post(logout_url)
-        verify_response = self.client.post(verify_url)
+        logout_response = self.with_token(token).logout_current()
+        verify_response = self.verify_token(token)
         self.assertEqual(verify_response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_logout_non_logged(self):
@@ -121,31 +151,26 @@ class APIAuthTest(APITestCase):
         This test verifies that the "logout" endpoint will reject request from non-logged clients
         :return:
         """
-        url = reverse('jwt_knox:logout_current_token')
-        response = self.client.post(url)
+        response = self.logout_current()
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_logout_old_token(self):
         """
-        Here we prove that a client can't use an old token to logout even if it logged in again
+        Here we prove that a client can't use an expired token to logout even
+        if logged in again with a different one
+
         :return:
         """
-        login_url = reverse('jwt_knox:get_new_token')
-        logout_url = reverse('jwt_knox:logout_current_token')
-        login_response = self.get_token()
-        first_token = login_response.data['token']
-        self.client.credentials(HTTP_AUTHORIZATION=self.build_auth_header(first_token))
-        self.client.post(logout_url)
-        response1 = self.client.post(login_url)
-        second_token = response1.data['token']
-        self.assertNotEqual(first_token, second_token)
-        response2 = self.client.post(logout_url)
+        token1 = self.get_token().data['token']
+        self.with_token(token1).logout_current()
+        token2 = self.get_token().data['token']
+        self.assertNotEqual(token1, token2)
+        response2 = self.with_token(token1).logout_current()
         self.assertEqual(response2.status_code, status.HTTP_401_UNAUTHORIZED)
 
         # We can eventually go on and verify if we're still logged
         # and if we can finally log out using the second token
-        self.client.credentials(HTTP_AUTHORIZATION=self.build_auth_header(second_token))
-        response3 = self.client.post(logout_url)
+        response3 = self.with_token(token2).logout_current()
         self.assertEqual(response3.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_verify(self):
@@ -153,11 +178,9 @@ class APIAuthTest(APITestCase):
         A simple test against or 'verify' endpoint
         :return:
         """
-        url = reverse('jwt_knox:verify_token')
         token_response = self.get_token()
         token = token_response.data['token']
-        self.client.credentials(HTTP_AUTHORIZATION=self.build_auth_header(token))
-        response = self.client.post(url)
+        response = self.verify_token(token)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_close_other_access(self):
@@ -166,20 +189,15 @@ class APIAuthTest(APITestCase):
         he's able to invalidate the other one using the 'logout_other' endpoint
         :return:
         """
-        login_url = reverse('jwt_knox:get_new_token')
-        verify_url = reverse('jwt_knox:verify_token')
-        logout_other = reverse('jwt_knox:logout_other_tokens')
         # Warning, in the next 3 lines we're using 'magic numbers'
         response_list = self.get_n_tokens(2)
         token1 = response_list[0].data['token']
         token2 = response_list[1].data['token']
-        self.client.credentials(HTTP_AUTHORIZATION=api_settings.JWT_AUTH_HEADER_PREFIX + ' ' + token1)
-        response3 = self.client.post(logout_other)
+        response3 = self.with_token(token1).logout_other()
         self.assertEqual(response3.status_code, status.HTTP_200_OK)
-        response4 = self.client.post(verify_url)
+        response4 = self.verify_token(token1)
         self.assertEqual(response4.status_code, status.HTTP_204_NO_CONTENT)
-        self.client.credentials(HTTP_AUTHORIZATION=api_settings.JWT_AUTH_HEADER_PREFIX + ' ' + token2)
-        response5 = self.client.post(verify_url)
+        response5 = self.verify_token(token2)
         self.assertEqual(response5.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_logout_all(self):
@@ -188,17 +206,13 @@ class APIAuthTest(APITestCase):
         related to a single user
         :return:
         """
-        verify_url = reverse('jwt_knox:verify_token')
-        logout_all_url = reverse('jwt_knox:logout_all_user_tokens')
         token_list = []
         response_list = self.get_n_tokens(self.test_logout_all_client_number)
         for i in range(0,self.test_logout_all_client_number):
             token = response_list[i].data['token']
             token_list.append(token)
-        self.client.credentials(HTTP_AUTHORIZATION=self.build_auth_header(token_list[0]))
-        logout_response = self.client.post(logout_all_url)
+        logout_response = self.with_token(token_list[0]).logout_all()
         self.assertEqual(logout_response.status_code, status.HTTP_204_NO_CONTENT)
         for i in range(0,self.test_logout_all_client_number):
-            self.client.credentials(HTTP_AUTHORIZATION=self.build_auth_header(token_list[i]))
-            response = self.client.post(verify_url)
+            response = self.verify_token(token_list[i])
             self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
